22f23153b0aa2aab0e412753ad7e2fc1535c44cc
AntoniaGrueber <Antonia.grueber@t-online.de>
2025-11-12 15:28:50 +0100

    h3Upload

---
+++ h3/H3_main.java
@@ -0,0 +1,15 @@
+package h3;
+
+public class H3_main {
+public static void main (String[] args) {
+	float celsiusFloat=14.2f;
+	float fahrenheitFloat;
+	double celsiusDouble=14.2;
+	double fahrenheitDouble;
+	
+	fahrenheitFloat= celsiusFloat*(9f/5f)+32f;
+	System.out.println("fahrenheitFloat:" + fahrenheitFloat);
+	fahrenheitDouble= celsiusDouble*(9.0/5.0)+32.0;
+	System.out.println("fahrenheitDouble: " + fahrenheitDouble);
+}
+}
