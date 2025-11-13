0bb511b3a65a95914722812be3c391f4a7820ea5
AntoniaGrueber <Antonia.grueber@t-online.de>
2025-11-13 15:24:12 +0100

    updateH3

--- h3/H3_main.java
+++ h3/H3_main.java
@@ -4,12 +4,13 @@ public class H3_main {
 public static void main (String[] args) {
 	float celsiusFloat=14.2f;
 	float fahrenheitFloat;
-	double celsiusDouble=14.2;
+	double celsiusDouble = celsiusFloat;
 	double fahrenheitDouble;
 	
 	fahrenheitFloat= celsiusFloat*(9f/5f)+32f;
 	System.out.println("fahrenheitFloat:" + fahrenheitFloat);
+	
 	fahrenheitDouble= celsiusDouble*(9.0/5.0)+32.0;
-	System.out.println("fahrenheitDouble: " + fahrenheitDouble);
+	System.out.println("fahrenheitDouble:" + fahrenheitDouble);
 }
 }
